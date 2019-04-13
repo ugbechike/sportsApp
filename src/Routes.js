1dd5e4a1d9982c39bcad52fc51104d1a13f83d2b
kidmeeno <arthurbosah@yahoo.com>
2019-04-13 11:34:25 +0100

    done with fixture and highlight page

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Component/Home/Home';
+import Fixture from './Component/fixture/fixture';
+import HighLights from './Component/highlights/highlights';
 
 class Routes extends Component {
   render() {
@@ -8,6 +10,8 @@ class Routes extends Component {
       <div>
         <Switch>
           <Route path="/" component={Home} exact />
+          <Route path="/fixture" component={Fixture}/>
+          <Route path="/highlights" component={HighLights}/>
         </Switch>
         
       </div>
