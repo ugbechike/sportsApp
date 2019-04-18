442b1a066c08752f6e33ebd317204a3470a77e2d
kidmeeno <arthurbosah@yahoo.com>
2019-04-18 17:36:01 +0100

    done with the fixture pages

--- src/Routes.js
+++ src/Routes.js
@@ -3,6 +3,7 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './Component/Home/Home';
 import Fixture from './Component/fixture/fixture';
 import HighLights from './Component/highlights/highlights';
+import MatchDetailCommentry from './Component/matchDetail(commentry)/matchDetail(commentry)';
 
 class Routes extends Component {
   render() {
@@ -12,6 +13,7 @@ class Routes extends Component {
           <Route path="/" component={Home} exact />
           <Route path="/fixture" component={Fixture}/>
           <Route path="/highlights" component={HighLights}/>
+          <Route path="/match-detail-commentry" component={MatchDetailCommentry}/>
         </Switch>
         
       </div>
